d16de3df3fca3ce1a7aab436a3046e668b9f8db9
soochan <tncksdl05@gmail.com>
2023-10-08 20:56:29 +0900

    item01: 8,9 버전에서 추가된 interface static, default 메서드

--- src/main/java/code/item01/HelloService.java
+++ src/main/java/code/item01/HelloService.java
@@ -1,5 +1,7 @@
 package code.item01;
 
+import java.util.Comparator;
+
 public interface HelloService {
 
     static HelloService of(String lang) {
@@ -9,6 +11,33 @@ public interface HelloService {
         return new KorHelloService();
     }
 
+    // 인스턴스를 호출하지 않아도 가능한 Method
+    static String hi() {
+        prepareMessage();
+        return "hi";
+    }
+
+    private static void prepareMessage() {
+    }
+
+    // 인스턴스를 호출하지 않아도 가능한 Method
+    static String hi1() {
+        prepareMessage();
+        return "hi";
+    }
+
+    // 인스턴스를 호출하지 않아도 가능한 Method
+    static String hi2() {
+        prepareMessage();
+        Comparator<Integer> desc = (o1, o2) -> o2 - o1;
+        return "hi";
+    }
+
     String sayHello();
+
+    // 인스턴스에서만 호출할 수 있는 Method (정의부 존재)
+    default String bye() {
+        return "bye";
+    }
 
 }
