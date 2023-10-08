7a4fd43956edfd3d30c05009c73be3206dc61782
soochan <tncksdl05@gmail.com>
2023-10-08 14:58:15 +0900

    item01: 인스턴스 생성 제어를 클래스 내부에서 제어 가능

--- src/main/java/code/item01/Settings.java
+++ src/main/java/code/item01/Settings.java
@@ -8,6 +8,7 @@ package code.item01;
 public class Settings {
 
     private static final Settings SETTINGS = new Settings();
+
     private boolean useAutoSteering;
     private boolean useABS;
     private Difficulty difficulty;
---
+++ src/main/java/code/item01/item01Main.java
@@ -0,0 +1,13 @@
+package code.item01;
+
+public class item01Main {
+
+    public static void main(String[] args) {
+        Settings settings1 = Settings.getInstance();
+        Settings settings2 = Settings.getInstance();
+
+        System.out.println(settings1);
+        System.out.println(settings2);
+    }
+
+}
